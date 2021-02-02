e53212ddf8d8b603dc46e576def7a849e5015524
joffreyverd <jverd@protonmail.com>
2021-02-02 21:59:26 +0100

    feature(common): improve strategy

--- strategy/common.py
+++ strategy/common.py
@@ -1,6 +1,6 @@
 //@version=4
 
-strategy(title='Common', overlay=true, initial_capital=1000, default_qty_type=strategy.percent_of_equity, default_qty_value=100, currency='EUR', commission_type=strategy.commission.percent, commission_value=0.26, pyramiding=10)
+strategy(title='Common', overlay=true, default_qty_type=strategy.cash, default_qty_value=1000, currency='EUR', commission_type=strategy.commission.percent, commission_value=0.26)
 
 start = timestamp(2020, 01, 01, 00, 00, 00)
 end = timestamp(2022, 01, 01, 00, 00, 00)
@@ -28,9 +28,6 @@ macd_src = input(title='MACD - Source', type=input.source, defval=close)
 signal_length = input(20, title='MACD - Signal Smoothing', type=input.integer, minval = 1, maxval = 50)
 sma_source = input(title='MACD - Simple MA(Oscillator)', type=input.bool, defval=false)
 sma_signal = input(title='MACD - Simple MA(Signal Line)', type=input.bool, defval=false)
-// Williams %R
-williams_length = input(title='Williams - Length', type=input.integer, defval=52)
-williams_src = input(close, 'Williams - Source', type = input.source)
 // OVERSELL - OVERBUY
 stoch_k_oversell = input(25, title='Stoch K - Oversell', type=input.integer, minval=1)
 stoch_rsi_k_oversell = input(25, title='Stoch RSI K - Oversell', type=input.integer, minval=1)
@@ -38,8 +35,6 @@ rsi_oversell = input(38, title='RSI - Oversell', type=input.integer, minval=1)
 stoch_k_overbuy = input(75, title='Stoch K - Overbuy', type=input.integer, minval=1)
 stoch_rsi_k_overbuy = input(75, title='Stoch RSI K - Overbuy', type=input.integer, minval=1)
 rsi_overbuy = input(62, title='RSI - Overbuy', type=input.integer, minval=1)
-williams_oversell = input(25, title='Williams - Oversell', type=input.integer, minval=1)
-williams_overbuy = input(75, title='Williams - Overbuy', type=input.integer, minval=1)
 
 
 // -------------- LOGIC -------------- //
@@ -53,17 +48,11 @@ rsi1 = rsi(src, lengthRSI)
 stoch_rsi_k = sma(stoch(rsi1, rsi1, rsi1, lengthStoch), smoothK)
 stoch_rsi_d = sma(stoch_rsi_k, smoothD)
 // MACD
-// fast_ma = sma_source ? sma(macd_src, fast_length) : ema(macd_src, fast_length)
-// slow_ma = sma_source ? sma(macd_src, slow_length) : ema(macd_src, slow_length)
-// macd = fast_ma - slow_ma
-// signal = sma_signal ? sma(macd, signal_length) : ema(macd, signal_length)
-// hist = macd - signal
-// Williams %R
-_pr(williams_length) =>
-	max = highest(williams_length)
-	min = lowest(williams_length)
-	100 * (williams_src - max) / (max - min)
-percentR = _pr(williams_length)
+fast_ma = sma_source ? sma(macd_src, fast_length) : ema(macd_src, fast_length)
+slow_ma = sma_source ? sma(macd_src, slow_length) : ema(macd_src, slow_length)
+macd = fast_ma - slow_ma
+signal = sma_signal ? sma(macd, signal_length) : ema(macd, signal_length)
+hist = macd - signal
 
 
 // -------------- STRATEGY -------------- //
@@ -78,21 +67,23 @@ if period and not openTrades
         buyCounter := buyCounter + 1
     if crossOver
         buyCounter := buyCounter + 1
-    if percentR < williams_overbuy
+    if macd < 0 and macd > macd[1]
         buyCounter := buyCounter + 1
 buySignal = buyCounter > 3
 
 sellCounter = 0
-if period and openTrades
+if period and openTrades and strategy.position_avg_price < open
     if stoch_k > stoch_k_overbuy and stoch_rsi_k > stoch_rsi_k_overbuy
         sellCounter := sellCounter + 2
     if rsi > rsi_overbuy
         sellCounter := sellCounter + 1
     if crossUnder
         sellCounter := sellCounter + 1
-    if percentR > williams_oversell
+    if macd > 0 and macd < macd[1]
         sellCounter := sellCounter + 1
 sellSignal = sellCounter > 3
 
-strategy.order('buy', true, 1, when = buySignal, comment='buy coef: ' + tostring(buyCounter))
-strategy.order('sell', false, 1, when = sellSignal, comment='sell coef: ' + tostring(sellCounter))
+strategy.order('buy', true, 1, when = buySignal, comment=tostring(close[0]))
+strategy.order('sell', false, 1, when = sellSignal, comment=tostring(close[0]))
+
+plot(strategy.position_avg_price, title = 'buy', color=color.red, linewidth = 2)
